bb96524222f03b54b7da7e05ada6ca6ef8784864
Ben <47262607+BenReichwein@users.noreply.github.com>
2020-08-16 13:22:11 -0400

    Update index.js
    daily and balance

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const bot = new Discord.Client();
 const token = "TOKEN";
 const fs = require('fs');
 let userData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-const prefix = '-';
+let prefix = '-';
+let dailytime = true;
 
 // LOGGING IN
 bot.once('ready', () => {
@@ -34,11 +35,11 @@ bot.on('message', message => {
     })
 
     // COMMANDS
-    let args = message.content.substring(prefix.length).split(" ");
+    let args = message.content.substring(prefix).split(" ");
 
     // 1 Word Commands - args[0]
     switch(args[0]) {
-        case 'help':
+        case `${prefix}help`:
             const help = new Discord.MessageEmbed()
                 .setTitle('HELP')
                 .setColor('#FF7F50')
@@ -46,24 +47,62 @@ bot.on('message', message => {
 
             message.channel.send(help);
             break;
-        case 'website':
+        case `${prefix}website`:
             const website = new Discord.MessageEmbed()
-                .setTitle('WEBSITE')
+                .setTitle('brainbusters.ca')
                 .setColor('#FF7F50')
                 .setURL('https://brainbusters.ca/')
 
             message.channel.send(website);
             break;
-        case 'stats':
+        case `${prefix}stats`:
             const stats = new Discord.MessageEmbed()
                 .setTitle('STATS')
                 .setColor('#FF7F50')
-                .setDescription('Coins: `'+ userData[sender.id].coins +'` \nMessages Sent: `'+ userData[sender.id].messagesSent +'`')
+                .setDescription('Coins: `$'+ userData[sender.id].coins +'` \nMessages Sent: `'+ userData[sender.id].messagesSent +'`')
 
             message.channel.send(stats);
             break;
+        case `${prefix}daily`:
+            if (dailytime === true) {
+                // Gives you 50-100 coins and writes it to the json file
+                let dailyreward = Math.floor(Math.random() * 50) + 50;
+                userData[sender.id].coins+=dailyreward;
+                fs.writeFile('data.json', JSON.stringify(userData), (err) => {
+                    if (err) console.error(err);
+                })
+                const daily = new Discord.MessageEmbed()
+                    .setTitle('DAILY')
+                    .setColor('#FF7F50')
+                    .setDescription('Reward: `$'+ dailyreward +'`\nBalance: `$'+ userData[sender.id].coins +'`')
 
+                message.channel.send(daily);
+                dailytime = false;
+                sleep(86400000).then(r => dailytime = true);
+            } else {
+                const dailyerror = new Discord.MessageEmbed()
+                    .setTitle('DAILY')
+                    .setColor('#FF4500')
+                    .setDescription('Daily reward available **TOMORROW!**')
+
+                message.channel.send(dailyerror);
+            }
+            break;
+        case `${prefix}balance`:case `${prefix}bal`:case `${prefix}coins`:
+            const balance = new Discord.MessageEmbed()
+                .setTitle('BALANCE')
+                .setColor('#FF7F50')
+                .setDescription('Coins: `$'+ userData[sender.id].coins +'`')
+
+            message.channel.send(balance);
+            break;
     }
 })
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
 
 bot.login(token);
