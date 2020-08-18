850bd08befd3f562279836b9b873741520d87c25
Ben <47262607+BenReichwein@users.noreply.github.com>
2020-08-17 22:06:38 -0400

    Update index.js
    Added CoinFlip (-cf, -coinflip)

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ bot.once('ready', () => {
     bot.user.setActivity(`${prefix}help | Benny#0917`);
 });
 
-bot.once('disconnect', () => {
-    console.log('[LOGGED OUT]');
-});
-
 
 // COMMANDS
 bot.on('message', message => {
@@ -34,11 +30,14 @@ bot.on('message', message => {
         if (err) console.error(err);
     })
 
-    // COMMANDS
+    // ARGUMENTS
     let args = message.content.substring(prefix).split(" ");
 
     // 1 Word Commands - args[0]
     switch(args[0]) {
+        
+        // HELP
+        
         case `${prefix}help`:
             const help = new Discord.MessageEmbed()
                 .setTitle('HELP')
@@ -47,6 +46,9 @@ bot.on('message', message => {
 
             message.channel.send(help);
             break;
+            
+            // WEBSITE
+            
         case `${prefix}website`:
             const website = new Discord.MessageEmbed()
                 .setTitle('brainbusters.ca')
@@ -55,6 +57,9 @@ bot.on('message', message => {
 
             message.channel.send(website);
             break;
+            
+            // STATS
+            
         case `${prefix}stats`:
             const stats = new Discord.MessageEmbed()
                 .setTitle('STATS')
@@ -63,6 +68,9 @@ bot.on('message', message => {
 
             message.channel.send(stats);
             break;
+            
+            // DAILY REWARD
+            
         case `${prefix}daily`:
             if (dailytime === true) {
                 // Gives you 50-100 coins and writes it to the json file
@@ -74,20 +82,23 @@ bot.on('message', message => {
                 const daily = new Discord.MessageEmbed()
                     .setTitle('DAILY')
                     .setColor('#FF7F50')
-                    .setDescription('Reward: `$'+ dailyreward +'`\nBalance: `$'+ userData[sender.id].coins +'`')
+                    .setDescription('You were awarded with `$'+ dailyreward +'`\n**Balance**: `$'+ userData[sender.id].coins +'`')
 
                 message.channel.send(daily);
                 dailytime = false;
                 sleep(86400000).then(r => dailytime = true);
             } else {
                 const dailyerror = new Discord.MessageEmbed()
-                    .setTitle('DAILY')
+                    .setTitle('SLOW DOWN THERE')
                     .setColor('#FF4500')
                     .setDescription('Daily reward available **TOMORROW!**')
 
                 message.channel.send(dailyerror);
             }
             break;
+            
+            // BALANCE
+            
         case `${prefix}balance`:case `${prefix}bal`:case `${prefix}coins`:
             const balance = new Discord.MessageEmbed()
                 .setTitle('BALANCE')
@@ -96,8 +107,56 @@ bot.on('message', message => {
 
             message.channel.send(balance);
             break;
+            
+            // COIN FLIP
+            
+        case `${prefix}cf`:case `${prefix}coinflip`:
+            if (args[1] < 50) {
+                const coinfliperr = new Discord.MessageEmbed()
+                    .setTitle(`BET TOO LOW`)
+                    .setColor('#FF4500')
+                    .setDescription('Bet must be more than $50')
+
+                message.channel.send(coinfliperr);
+            } else if(!args[1]) {
+                const coinfliperr1 = new Discord.MessageEmbed()
+                    .setTitle(`ERROR`)
+                    .setColor('#FF4500')
+                    .setDescription('Give an amount to bet')
+
+                message.channel.send(coinfliperr1);
+            } else if(args[1] > userData[sender.id].coins) {
+                const coinfliperr2 = new Discord.MessageEmbed()
+                    .setTitle(`INSUFFICIENT FUNDS`)
+                    .setColor('#FF4500')
+                    .setDescription('You do not have `$' + args[1] + '`')
+
+                message.channel.send(coinfliperr2);
+            } else if(args[1] >= 50) {
+                let flip = Math.floor(Math.random() * 2) + 1
+                if (flip === 1) {
+                    const cfwin = new Discord.MessageEmbed()
+                        .setTitle('COIN FLIP')
+                        .setColor('#7FFF00')
+                        .setDescription('***50/50*** **Chance**\n\nYou won: `$' + args[1] + '`\n**Balance**: `$'+ userData[sender.id].coins +'`')
+
+                    message.channel.send(cfwin);
+                } else {
+                    userData[sender.id].coins-=args[1];
+                    fs.writeFile('data.json', JSON.stringify(userData), (err) => {
+                        if (err) console.error(err);
+                    })
+                    const cflose = new Discord.MessageEmbed()
+                        .setTitle('COIN FLIP')
+                        .setColor('#FF4500')
+                        .setDescription('***50/50*** **Chance**\n\nYou lost: `$' + args[1] + '`\n**Balance**: `$'+ userData[sender.id].coins +'`')
+
+                    message.channel.send(cflose);
+                }
+            }
+            break;
     }
-})
+});
 
 function sleep(ms) {
     return new Promise((resolve) => {
